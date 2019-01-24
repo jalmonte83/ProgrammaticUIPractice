4ae10273167f4d96bf90f6aa421b88e8302bbebb
Jeffrey Almonte <jalmonte83@gmail.com>
2019-01-24 17:27:50 -0500

    first try

---
+++ ProgrammaticUIPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProgrammaticUIPractice
+//
+//  Created by Jeffrey Almonte on 1/24/19.
+//  Copyright © 2019 Jeffrey Almonte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
